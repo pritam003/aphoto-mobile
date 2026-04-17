4398f4d5c42669091c2772924aec1b9b4a5cbcd0
pritam prasad <pritamprasad@192.168.1.2>
2026-04-17 09:33:36 +0530

    fix: add react-native-worklets dep and babel plugins for reanimated v4

--- artifacts/aphoto-native/babel.config.js
+++ artifacts/aphoto-native/babel.config.js
@@ -5,5 +5,9 @@ module.exports = function (api) {
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
+    plugins: [
+      "react-native-worklets/plugin",
+      "react-native-reanimated/plugin",
+    ],
   };
 };
